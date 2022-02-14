44f00f051f5501d80a3fd6434c539d5bf31359f9
Brian Vu <brianvu7@gmail.com>
2022-02-14 17:35:55 -0600

    added functions

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+const mapImg = document.querySelector("#map-image");
+const cityName = document.querySelector("#city-name");
+
 const getPOIs = () => {
     fetch("https://www.triposo.com/api/20220104/location.json?part_of=United_States&tag_labels=city&count=10&order_by=-score&fields=name,id,snippet,parent_id,score,type,images,coordinates&account=7GPWA5CT&token=8w8tduvc82ln7ebbx42bd1ugcd6hxbcw")
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => renderCitiesNav(data.results));
 }
 
-const getPlacesToEat = () => {
-    fetch("https://www.triposo.com/api/20220104/poi.json?location_id=New_York_City&tag_labels=eatingout&count=10&fields=id,name,score,intro,tag_labels,best_for,images&order_by=-score&account=7GPWA5CT&token=8w8tduvc82ln7ebbx42bd1ugcd6hxbcw")
+const getPlacesToEat = cityID => {
+    fetch(`https://www.triposo.com/api/20220104/poi.json?location_id=${cityID}&tag_labels=eatingout&count=10&fields=id,name,score,intro,tag_labels,best_for,images,snippet&order_by=-score&account=7GPWA5CT&token=8w8tduvc82ln7ebbx42bd1ugcd6hxbcw`)
     .then(res => res.json())
     .then(data => {
-        console.log(data)
-        data.results.forEach(restaurant => document.body.append(restaurant.name))
+        console.log(data);
+        return data;
     });
 }
 
@@ -25,7 +28,27 @@ const getLocal = () => {
     .then(data => console.log(data));
 }
 
-//getPOIs();
+const renderCitiesNav = cities => {
+    console.log(cities);
+    cities.forEach(city => {
+        const cityDiv = document.createElement("div");
+        const cityName = document.createElement("h1");
+        const cityImg = document.createElement("img");
+        
+        cityName.textContent = city.name;
+        cityImg.src = city.images[0].sizes.thumbnail.url;
+
+        cityDiv.addEventListener("click", () => {
+            console.log(city);
+            mapImg.src = city.images[0].sizes.medium.url;
+        })
+
+        cityDiv.append(cityName, cityImg);
+        document.body.append(cityDiv);
+    })
+}
+
+getPOIs();
 //getPlacesToEat();
 //getAttractions();
 //getLocal();
